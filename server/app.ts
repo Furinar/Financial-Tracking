ee1e51523a42751f67adca0f2cffdfb3be9582dd
Furina <3559551198@qq.com>
2026-01-22 21:01:45 +0800

    server端ts化

---
+++ server/app.ts
@@ -0,0 +1,49 @@
+import createError, { HttpError } from 'http-errors';
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+// User router does not seem to be used in original app.js even though users.js existed.
+// Wait, looking at original app.js content:
+// app.use('/', indexRouter);
+// There was NO usage of users router in the provided app.js content call! 
+// Let me double check the `read_file` output from earlier.
+// Output:
+// var indexRouter = require('./routes/index');
+// app.use('/', indexRouter);
+// It seems `users` router was not imported in the truncated `read_file` or simply not used.
+// Let me read FULL app.js first to be 100% sure I'm not missing imports.
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+export default app;
